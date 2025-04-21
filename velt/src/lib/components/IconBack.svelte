2567c30f86901c4bb1cd06a415de1fa73ed773d9
sungminna <sungmin.na330@gmail.com>
2025-04-22 00:36:48 +0900

    feat: icon rendering updated

---
+++ velt/src/lib/components/IconBack.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export let size: number | string = 24;
+  export let color: string = 'currentColor';
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="{color}"
+  width="{size}"
+  height="{size}"
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M15 19l-7-7 7-7"
+  />
+</svg> 
---
+++ velt/src/lib/components/IconSend.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let size: number | string = 24;
+  export let color: string = 'currentColor';
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  viewBox="0 0 24 24"
+  fill="{color}"
+  width="{size}"
+  height="{size}"
+>
+  <path d="M3.478 2.405a.75.75 0 0 0-.926.94l2.432 7.905H13.5a.75.75 0 0 1 0 1.5H4.984l-2.432 7.905a.75.75 0 0 0 .926.94 60.519 60.519 0 0 0 18.445-8.986.75.75 0 0 0 0-1.218A60.517 60.517 0 0 0 3.478 2.405Z"/>
+</svg> 
